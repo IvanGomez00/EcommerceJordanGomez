b89f7f202d92bf671fa2ef5f90ef1e6bfdbd0a94
Ivan Gomez <ivangomezdeveloper@gmail.com>
2023-10-02 23:26:16 -0300

    subiendo a github

---
+++ js/main.js
@@ -0,0 +1,14 @@
+    let zapatillas = [
+        {id:1, nombre:"Air Jordan 1 Mid SE", genero: "hombre", precio:99.999, descripcion:"Lleva la energía Jordan en los pies este invierno. El exquisito cuero de pleno grano con detalles brillantes hacen que este par brille como las luces navideñas.",
+       imagen:"https://nikearprod.vtexassets.com/arquivos/ids/702810-800-800?v=638233933446270000&width=800&height=800&aspect=true"}
+       {id:2, nombre:"Jordan Stadium 90", genero: "hombre", precio:99.999, descripcion:"La comodidad es lo más importante, pero eso no significa que debas sacrificar el estilo. El Stadium 90, con un diseño inspirado en el AJ1 y el AJ5, está listo para usarlo todos los días.",
+       imagen:"https://nikearprod.vtexassets.com/arquivos/ids/680938-800-800?v=638224561267800000&width=800&height=800&aspect=true"}
+       {id:3, nombre:"Jordan Series .05", genero: "hombre", precio:62.999, descripcion:"Entra en la leyenda de MJ con el Jordan Series .05. Estos clásicos instantáneos, que se pueden usar en cualquier escenario, ofrecen líneas definidas y buenos looks.",
+       imagen:"https://nikearprod.vtexassets.com/arquivos/ids/156783-800-800?v=638086296641670000&width=800&height=800&aspect=true"}
+       {id:4, nombre:"Jordan Delta 3 Low", genero: "mujer", precio:83.199, descripcion:"Este calzado es ideal para brillar en la cancha, en la calle y en todo el mundo. El cuero sintético y los materiales de tela se combinan con un interior sin costuras.",
+       imagen:"https://nikearprod.vtexassets.com/arquivos/ids/629191-800-800?v=638210594179700000&width=800&height=800&aspect=true"}
+       {id:5, nombre:"Air Jordan 11 Retro Low", genero: "mujer", precio:136.999, descripcion:"El diseño legendario del Air Jordan 11 Retro Low redefine el estilo del básquetbol. La unidad de amortiguación de largo completo ofrece una mayor comodidad a este modelo icónico.",
+       imagen:"https://nikearprod.vtexassets.com/arquivos/ids/702544-800-800?v=638233926738070000&width=800&height=800&aspect=true"}
+       {id:6, nombre:"Air Jordan 1 Acclimate", genero: "mujer", precio:87.199, descripcion:"Este diseño mitad bota y mitad calzado deportivo está diseñado con materiales acondicionados para el invierno, con una reforma del talón a la punta que mantiene la calidez y te protege de la humedad sin perder el atractivo icónico del AJ1.",
+       imagen:"https://nikearprod.vtexassets.com/arquivos/ids/451888-1000-1000?v=638149276309670000&width=1000&height=1000&aspect=true"}
+    ]
